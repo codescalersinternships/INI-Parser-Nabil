5318724562ece5e39ae5be2816602c76e854585a
Nabil Salah <nabil.salah203@gmail.com>
2024-06-27 15:11:10 +0300

    fix: unhandled error in main

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	iniparser "github.com/codescalersinternships/INI-Parser-Nabil/pkg"
+	_ "github.com/codescalersinternships/INI-Parser-Nabil/pkg"
 )
 
 func main() {
-	parser := iniparser.NewIniParser()
-	parser.LoadFromFile("./input.ini")
-	fmt.Println(parser.SaveToFile("Hello.ini"))
+
 }
